9028ec5ff387d665aff9c9624e37c567732b42eb
Hugo ARNAL <hugo.arnal@epitech.eu>
2025-08-13 18:36:00 +0200

    feat: clap cli init

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
+mod commands;
 mod database;
 mod providers;
 mod utils;
 
 use std::process;
 
+use clap::{Command, command};
 use dotenv::dotenv;
 use log;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let matches = command!()
+        .subcommand(Command::new("connect").about("Connect to OAuth provider platforms"))
+        .get_matches();
+
     env_logger::init_from_env(
         env_logger::Env::default().filter_or(env_logger::DEFAULT_FILTER_ENV, "info"),
     );
@@ -25,10 +31,18 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
     }
 
-    let mut provider = providers::new(platform.unwrap());
-    provider.connect().await;
-    loop {
-        provider.currently_playing().await;
-        provider.wait();
+    match matches.subcommand() {
+        Some(("connect", _)) => {
+            commands::connect::connect();
+            Ok(())
+        }
+        _ => {
+            let mut provider = providers::new(platform.unwrap());
+            provider.connect().await;
+            loop {
+                provider.currently_playing().await;
+                provider.wait();
+            }
+        }
     }
 }
